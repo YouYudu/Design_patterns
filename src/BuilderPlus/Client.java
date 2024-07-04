1ff1c5545319a0cba2992b47b24991ea9c78caf5
youyudu <nju_yyd@163.com>
2024-07-05 00:01:35 +0800

    BuilderPlus

---
+++ src/BuilderPlus/Client.java
@@ -0,0 +1,13 @@
+package BuilderPlus;
+
+public class Client {
+    public static void main(String[] args) {
+        Phone phone = new Phone.Builder()
+                .cpu("intel")
+                .mainBoard("华硕")
+                .memory("金士顿")
+                .screen("三星")
+                .build();
+        System.out.println(phone);
+    }
+}
---
+++ src/BuilderPlus/Phone.java
@@ -0,0 +1,59 @@
+package BuilderPlus;
+
+public class Phone {
+    private String cpu;
+    private String screen;
+    private String memory;
+    private String mainBoard;
+
+    protected Phone (Builder builder) {
+        cpu = builder.cpu;
+        screen = builder.screen;
+        memory = builder.memory;
+        mainBoard = builder.mainBoard;
+    }
+
+    public static final class Builder {
+        private String cpu;
+        private String screen;
+        private String memory;
+        private String mainBoard;
+
+        public Builder() {}
+
+        public Builder cpu(String val) {
+            cpu = val;
+            return this;
+        }
+
+        public Builder screen(String val) {
+            screen = val;
+            return this;
+        }
+
+        public Builder memory(String val) {
+            memory = val;
+            return this;
+        }
+
+        public Builder mainBoard(String val) {
+            mainBoard = val;
+            return this;
+        }
+
+        public Phone build() {
+            return new Phone(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "cpu='" + cpu + '\'' +
+                ", screen='" + screen + '\'' +
+                ", memory='" + memory + '\'' +
+                ", mainboard='" + mainBoard + '\'' +
+                '}';
+    }
+
+}
